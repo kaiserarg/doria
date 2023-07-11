b467394715d7431f00644ed6ec45e79a89624ddd
Zhifeng Yao <zyao2003@gmail.com>
2023-07-11 02:47:42 -0400

    grid system
    work on the grid system a bit more

--- client/src/components/SubscriptionWidget.js
+++ client/src/components/SubscriptionWidget.js
@@ -15,28 +15,32 @@ const SubscriptionWidget = () => {
 
     return (
         <form onSubmit={{handleSubmit}}>
-            <div className='wrapper'>
+            <div className='wrapper-1'>
                 <div className='text-0'>
                     <img className='logo' src={dorialogo} alt="Doria Logo" />
                     <img className='title' src={doriatext} alt="Doria Text" />
                 </div>
-                <div className='mainBody'>
-                    <h2 className='text-1'>Doria | Web-based PvP Games</h2>
-                    <h1 className='text-2'>Doria is a website that host a collection of online PVP games</h1>
-                    <h1 className='text-3'>Join the waitlist</h1>
-                    <h3 className='text-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
-                    <input
-                        className='emailInput'
-                        name='Email'
-                        placeholder='Email'
-                        type='email'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <br/>
-                    <button className='btn' type='submit'>Subscribe</button>
+                    <div className='subWrapper-1'>
+                        <div className='text-1'>Doria | Web-based PvP Games</div>
+                        <div className='text-2'>Doria is a website that host a collection of online PVP games</div>
+                        <div className='text-3'>Join the waitlist</div>
+                    </div>
+                    <div className='text-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                    <div className='subWrapper-2'>
+                        <div className='emailInput'>
+                            <input
+                                name='Email'
+                                placeholder='Email'
+                                type='email'
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className='btn'>
+                            <button type='submit'>Subscribe</button>
+                        </div> 
+                    </div>
                 </div>
-            </div>
         </form>
     );
 };
